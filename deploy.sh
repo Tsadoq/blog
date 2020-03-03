95b9e2d45a7dbb53875180dd4dc81da680cc9926
Matteo Villosio <sirjack9601@gmail.com>
2020-03-03 16:03:20 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+rm -r public/
+git submodule add -f -b master https://github.com/tsadoq/tsadoq.github.io.git public
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+hugo
+cd public
+git add .
+git commit -m "Build website"
+git push origin master
+cd ..
+
+echo 'finished'
